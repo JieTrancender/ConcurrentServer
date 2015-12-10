ed05ca30b61fccb1a10546884b9098fc2124f5a8
_Trancender <trancender@vip.qq.com>
2015-12-10 23:42:38 +0800

    Create thread-for-member.cpp

---
+++ concurrency-program-practice/manage-process/thread-for-member.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <thread>
+
+class Wrapper
+{
+public:
+	void member1()
+	{
+		std::cout << "i am member1" << std::endl;
+	}
+
+	void member2(const char *arg1, unsigned arg2)
+	{
+		std::cout << "i am member2 and first arg is (" << arg1 << "and second arg is " << arg2 << std::endl;
+	}
+
+	std::thread member1Thread()
+	{
+		return std::thread([=]{member1(); }); //lambda
+	}
+
+	std::thread member2Thread(const char *arg1, unsigned arg2)
+	{
+		return std::thread([=]{member2(arg1, arg2); });
+	}
+};
+
+int main(int argc, char **argv)
+{
+	Wrapper *w = new Wrapper();
+	std::thread w1 = w->member1Thread();
+	std::thread w2 = w->member2Thread("hello", 100);
+
+	w1.join();
+	w2.join();
+
+	return 0;
+}
